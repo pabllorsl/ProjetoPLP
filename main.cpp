3beb84517c1c7c22ba2faa4a61430473cc10f757
fabianaag <fabiana.gomes@ccc.ufcg.edu.br>
2018-05-23 21:56:55 -0300

    Questões adicionadas e aleatoriedade delas incluída

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@
 
 using namespace std;
 
-vector<string> faceis;
-vector<string> questao;
+vector<vector<string>> questoesFaceisCG;
+vector<string> questaoFacilUmCG;
+vector<string> questaoFacilDoisCG;
+vector<string> questaoFacilTresCG;
 vector<int> premios;
 
 string nome;
 string resposta;
 bool acertou;
 int premioAtual = 0;
+int indiceAleatorio;
 
 int pulos = 3;
 int cartas = 1;
@@ -21,9 +24,7 @@ int placas = 1;
 
 void iniciarJogo();
 
-void preencherPerguntasFaceis();
-
-int perguntaAleatoria(const vector<string> &vector);
+int perguntaAleatoria(vector<vector<string>> &vector);
 
 void pararJogo();
 
@@ -37,7 +38,11 @@ void preencherPremios();
 
 void imprimirPremios();
 
-void preencherQuestao();
+void preencherQuestaoFacilUmCG();
+
+void preencherQuestaoFacilDoisCG();
+
+void preencherQuestaoFacilTresCG();
 
 void imprimirQuestao();
 
@@ -55,10 +60,6 @@ void atualizarPremio();
 
 int main() {
     iniciarJogo();
-
-    /*preencherPerguntasFaceis();
-    cout << faceis.at(perguntaAleatoria(faceis)) << endl;*/
-
     return 0;
 }
 
@@ -70,7 +71,10 @@ void iniciarJogo() {
     cin >> nome;
 
     preencherPremios();
-    preencherQuestao();
+    preencherQuestaoFacilUmCG();
+    preencherQuestaoFacilDoisCG();
+    preencherQuestaoFacilTresCG();
+
 //    imprimirPremios();
     imprimirQuestao();
     escolherAlternativa();
@@ -92,20 +96,47 @@ void imprimirPremios() {
     }
 }
 
-void preencherQuestao() {
-    questao.push_back("Quanto sao 2+2?");
-    questao.push_back("A) 5");
-    questao.push_back("B) 7");
-    questao.push_back("C) 4");
-    questao.push_back("D) 2");
-    questao.push_back("C");
-    questao.push_back("c");
+void preencherQuestaoFacilUmCG() {
+    questaoFacilUmCG.push_back("Quanto sao 2+2?");
+    questaoFacilUmCG.push_back("A) 5");
+    questaoFacilUmCG.push_back("B) 7");
+    questaoFacilUmCG.push_back("C) 4");
+    questaoFacilUmCG.push_back("D) 2");
+    questaoFacilUmCG.push_back("C");
+    questaoFacilUmCG.push_back("c");
+
+    questoesFaceisCG.push_back(questaoFacilUmCG);
+}
+
+void preencherQuestaoFacilDoisCG() {
+    questaoFacilDoisCG.push_back("O que esta escrito na bandeira do Brasil?");
+    questaoFacilDoisCG.push_back("A) Ordem e Regresso");
+    questaoFacilDoisCG.push_back("B) Ordem e Retrocesso");
+    questaoFacilDoisCG.push_back("C) Ordem e Progresso");
+    questaoFacilDoisCG.push_back("D) Progresso e Ordem");
+    questaoFacilDoisCG.push_back("C");
+    questaoFacilDoisCG.push_back("c");
+
+    questoesFaceisCG.push_back(questaoFacilDoisCG);
+}
+
+void preencherQuestaoFacilTresCG() {
+    questaoFacilTresCG.push_back("Qual o significado da palavra inglesa 'horse'?");
+    questaoFacilTresCG.push_back("A) Casa");
+    questaoFacilTresCG.push_back("B) Cavalo");
+    questaoFacilTresCG.push_back("C) Elefante");
+    questaoFacilTresCG.push_back("D) Hipopótamo");
+    questaoFacilTresCG.push_back("B");
+    questaoFacilTresCG.push_back("b");
+
+    questoesFaceisCG.push_back(questaoFacilTresCG);
 }
 
 void imprimirQuestao() {
     cout << "\nPergunta valendo: R$" << premios.at(premioAtual) << endl;
-    for (int i = 0; i < questao.size() - 2; ++i) {
-        cout << questao.at(i) << endl;
+    indiceAleatorio = perguntaAleatoria(questoesFaceisCG);
+    for (int i = 0; i < questoesFaceisCG.at(indiceAleatorio).size() - 2; ++i) {
+        cout << questoesFaceisCG.at(indiceAleatorio).at(i) << endl;
     }
 }
 
@@ -116,7 +147,8 @@ string escolherAlternativa() {
 }
 
 bool alternativaCorreta() {
-    if (questao.at(5).compare(resposta) == 0 || questao.at(6).compare(resposta) == 0) {
+    if (questoesFaceisCG.at(indiceAleatorio).at(5).compare(resposta) == 0 ||
+        questoesFaceisCG.at(indiceAleatorio).at(6).compare(resposta) == 0) {
         acertou = true;
         return true;
     } else {
@@ -161,13 +193,8 @@ void atualizarPremio() {
 Medias: 50mil, 100 mil
 Dificeis: 500 mil, 1milhao*/
 
-/*void preencherPerguntasFaceis() {
-    faceis.push_back("Quem eh o presidente do Brasil?");
-    faceis.push_back("Quem sou eu?");
-    faceis.push_back("O que eh binario?");
-}*/
 
-int perguntaAleatoria(const vector <string> &vector) {
+int perguntaAleatoria(vector<vector<string>> &vector) {
     srand(time(nullptr));
     return rand() % vector.size();
 }
